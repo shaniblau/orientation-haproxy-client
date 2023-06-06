af3241f9f6e258e4a16a06e306490dfaef44a15a
שני בלאו <shaniblau@snys-MacBook-Air.local>
2023-06-06 10:52:55 +0300

    change to only one file at a time

--- send_files.py
+++ send_files.py
@@ -6,9 +6,10 @@ import requests
 def upload_file():
     url = 'http://20.185.67.127:8000/uploadfile'
     files = create_files("../dogs")
-    server = requests.post(url, files=files)
-    output = server.text
-    print('The response from the server is:\n', output)
+    for file in files:
+        server = requests.post(url, files=file)
+        output = server.text
+        print('The response from the server is:\n', output)
 
 
 def create_files(directory: str):
@@ -16,7 +17,7 @@ def create_files(directory: str):
     files_names: list = os.listdir(directory)
     for name in files_names:
         file_path = os.path.join(directory, name)
-        files.append(('files', (name, open(file_path, 'rb'), 'image/jpeg')))
+        files.append({'files', open(file_path, 'rb')})
     return files
 
 
